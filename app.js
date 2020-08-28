30a49d9afe41fdca3645d126e6dee07358eb955e
Felix Mendoza <23099832+mendoza12b@users.noreply.github.com>
2020-08-28 13:08:33 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,140 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {graphqlHTTP} = require('express-graphql');
+const { buildSchema }= require('graphql');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const Event = require('./models/event');
+const User = require('./models/user');
+const { populate } = require('./models/event');
+
+const app = express(); 
+
+app.use(bodyParser.json());
+
+app.use(
+    '/graphql',
+ graphqlHTTP({
+    schema: buildSchema(`
+        type Event {
+            _id: ID!
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+            creator: User!
+        }
+         
+        type User {
+            _id:ID!
+            email: String!
+            password: String
+            createdEvents: [Event!]
+        }
+
+        input EventInput {
+            title: String!
+            description: String!
+            price: Float!
+            date: String!
+        }
+         
+        input UserInput {
+            email:String! 
+            password: String!
+        }
+
+        type RootQuery {
+            events: [Event!]!
+        }
+
+        type RootMutation { 
+            createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
+        }
+
+        schema {
+            query: RootQuery  
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+           return Event.find()
+           .populate('creator')
+           .then(events => {
+            return events.map(event => { 
+                return {...event._doc, _id: event.id };
+            });
+           })
+           .catch(err => {
+               throw err;
+           });
+        },
+        createEvent: args => {
+            const event = new Event({
+                title: args.eventInput.title,
+                description: args.eventInput.description, 
+                price: +args.eventInput.price,
+                date: new Date(args.eventInput.date),
+                creator: '5f49176aa5d4ae5728313f24'
+            });
+            let createdEvent;
+            return event
+            .save()
+            .then(result => {
+                createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+                return User.findById('5f49176aa5d4ae5728313f24')
+            })
+            .then(user => {
+                 if (!user) {
+                     throw new Error('User not found');
+                 }
+                 user.createdEvents.push(event);
+                 return user.save();
+            })
+            .then(result => {
+                return createdEvent;
+            })
+            .catch( err => {
+                console.log(err);
+                throw err;
+            });
+            
+        },
+        createUser: args => {
+            return User.findOne({ email: args.userInput.email })
+            .then(user => {
+                if (user) {
+                    throw new Error('User exists already.');
+                }
+                return bcrypt.hash(args.userInput.password, 12);
+            })
+            .then(hashedPassword => {
+                const user = new User({
+                    email: args.userInput.email,
+                    password: hashedPassword
+                });
+                return user.save();
+            })
+            .then( result => {
+                return {...result._doc,password: null, _id: result.id};
+            })
+
+            . catch(err => {
+                throw err;
+            }); 
+        }
+    },
+    graphiql: true
+  })
+);
+
+mongoose
+.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.i5krb.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
+ .then(() => {
+     app.listen(3000);
+ }).catch(err => {
+     console.log(err);
+ });
