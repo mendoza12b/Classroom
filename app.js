83ad6aa01fca12989973697d7856229ec39de07e
Felix <mendoza.felix@outlook.com>
2020-08-31 12:48:47 -0400

    not showing creator bug

--- app.js
+++ app.js
@@ -65,7 +65,13 @@ app.use(
            .populate('creator')
            .then(events => {
             return events.map(event => { 
-                return {...event._doc, _id: event.id };
+                return {...event._doc,
+                     _id: event.id ,
+                     creator: {
+                         ...event.doc.creator._doc,
+                         _id: event._doc.creator.id
+                     }
+                };
             });
            })
            .catch(err => {
